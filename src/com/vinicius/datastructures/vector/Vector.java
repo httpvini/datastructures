2f92fed11d9c0d533bf1d2a567d3897a568f1d6f
Vinicius Oliveira <vinicius.olliveira@outlook.com>
2018-09-03 17:17:47 -0300

    Início de implementação da abstração do vetor para reutilização na aplicação.

--- src/com/vinicius/datastructures/vector/Vector.java
+++ src/com/vinicius/datastructures/vector/Vector.java
@@ -1,4 +1,19 @@
 package com.vinicius.datastructures.vector;
 
 public class Vector {
+
+    private String[] elementos;
+
+    public Vector(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+
+    public void adiciona(String elemento){
+        for (int i=0; i<this.elementos.length; i++){
+            if (this.elementos[i] == null){
+                this.elementos[i] = elemento;
+                break;
+            }
+        }
+    }
 }
--- src/com/vinicius/datastructures/vector/teste/VectorTest.java
+++ src/com/vinicius/datastructures/vector/teste/VectorTest.java
@@ -1,4 +1,17 @@
 package com.vinicius.datastructures.vector.teste;
 
+import com.vinicius.datastructures.vector.Vector;
+
 public class VectorTest {
+
+    public static void main(String[] args) {
+        Vector vector = new Vector(5);
+        vector.adiciona("elemento 1");
+        vector.adiciona("elemento 2");
+
+
+    }
+
+
+
 }
